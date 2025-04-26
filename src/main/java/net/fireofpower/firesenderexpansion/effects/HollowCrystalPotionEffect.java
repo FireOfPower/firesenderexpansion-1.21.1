bd9fb169073d84070e27df628d1e5d7c03b158a8
Fire <fireofpower333@gmail.com>
2025-04-26 16:29:07 -0400

    The users eyes now glow when charging Hollow Crystal

--- src/main/java/net/fireofpower/firesenderexpansion/effects/HollowCrystalPotionEffect.java
+++ src/main/java/net/fireofpower/firesenderexpansion/effects/HollowCrystalPotionEffect.java
@@ -1,10 +1,25 @@
 package net.fireofpower.firesenderexpansion.effects;
 
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.capabilities.magic.SyncedSpellData;
 import io.redspace.ironsspellbooks.effect.MagicMobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 
 public class HollowCrystalPotionEffect extends MagicMobEffect {
     public HollowCrystalPotionEffect(){
         super(MobEffectCategory.BENEFICIAL, 5984177);
+    }
+
+    @Override
+    public void onEffectAdded(LivingEntity pLivingEntity, int pAmplifier) {
+        super.onEffectAdded(pLivingEntity, pAmplifier);
+        MagicData.getPlayerMagicData(pLivingEntity).getSyncedData().addEffects(SyncedSpellData.PLANAR_SIGHT);
+    }
+
+    @Override
+    public void onEffectRemoved(LivingEntity pLivingEntity, int pAmplifier) {
+        super.onEffectRemoved(pLivingEntity, pAmplifier);
+        MagicData.getPlayerMagicData(pLivingEntity).getSyncedData().removeEffects(SyncedSpellData.PLANAR_SIGHT);
     }
 }
