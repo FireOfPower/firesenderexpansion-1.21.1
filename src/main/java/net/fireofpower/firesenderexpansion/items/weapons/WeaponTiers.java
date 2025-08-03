d65ac16b3e145533b07f28eb498cb227fbe241c1
FireOfPower <fireofpower333@gmail.com>
2025-08-02 20:41:04 -0700

    adjusted damage of TBATE

--- src/main/java/net/fireofpower/firesenderexpansion/items/weapons/WeaponTiers.java
+++ src/main/java/net/fireofpower/firesenderexpansion/items/weapons/WeaponTiers.java
@@ -15,7 +15,7 @@ import java.util.function.Supplier;
 
 public class WeaponTiers implements Tier, IronsWeaponTier {
 
-    public static WeaponTiers VOID_STAFF = new WeaponTiers(2031,1F,-2.6F,15, BlockTags.INCORRECT_FOR_NETHERITE_TOOL,() -> Ingredient.of(ItemRegistry.MITHRIL_INGOT.get()),
+    public static WeaponTiers VOID_STAFF = new WeaponTiers(2031,7F,-2.6F,15, BlockTags.INCORRECT_FOR_NETHERITE_TOOL,() -> Ingredient.of(ItemRegistry.MITHRIL_INGOT.get()),
             new AttributeContainer(AttributeRegistry.COOLDOWN_REDUCTION, 0.15, AttributeModifier.Operation.ADD_MULTIPLIED_BASE),
             new AttributeContainer(AttributeRegistry.SPELL_POWER, 0.1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE),
             new AttributeContainer(AttributeRegistry.ENDER_SPELL_POWER, 0.1, AttributeModifier.Operation.ADD_MULTIPLIED_BASE),
