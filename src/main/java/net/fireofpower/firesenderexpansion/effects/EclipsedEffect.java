fbc1713f396cc94c31ad9f2ac6b7a7a9d7e66507
FireOfPower <fireofpower333@gmail.com>
2026-02-23 01:54:39 -0500

    Revamped Eclipsed to be more effient, accurate, etc.

--- src/main/java/net/fireofpower/firesenderexpansion/effects/EclipsedEffect.java
+++ src/main/java/net/fireofpower/firesenderexpansion/effects/EclipsedEffect.java
@@ -2,6 +2,7 @@ package net.fireofpower.firesenderexpansion.effects;
 
 import io.redspace.ironsspellbooks.effect.MagicMobEffect;
 import net.fireofpower.firesenderexpansion.FiresEnderExpansion;
+import net.fireofpower.firesenderexpansion.registries.EffectRegistry;
 import net.fireofpower.firesenderexpansion.util.Utils;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -10,39 +11,42 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@EventBusSubscriber
 public class EclipsedEffect extends MagicMobEffect {
+
+    private static final Map<LivingEntity,Integer> buffTracking = new HashMap<>();
+
     public EclipsedEffect() {
         super(MobEffectCategory.HARMFUL, Utils.rgbToInt(0,0,0));
     }
 
-    @Override
-    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-        return true;
+    public static void updateAttributeModifiers(LivingEntity pLivingEntity) {
+        AttributeInstance instanceAtkSpeed = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_SPEED);
+        AttributeInstance instanceAtkPower = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_DAMAGE);
+
+        if (instanceAtkPower != null && instanceAtkSpeed != null && buffTracking.containsKey(pLivingEntity)) {
+            instanceAtkPower.addOrUpdateTransientModifier(new AttributeModifier(FiresEnderExpansion.id("eclipsed.power"), buffTracking.get(pLivingEntity) * -0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+            instanceAtkSpeed.addOrUpdateTransientModifier(new AttributeModifier(FiresEnderExpansion.id("eclipsed.speed"), buffTracking.get(pLivingEntity) * -0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+        }
     }
 
-    @Override
-    public boolean applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        if (pLivingEntity.tickCount % 20 == 0) {
-            int numPositiveEffects = 0;
-            for (MobEffectInstance instance : pLivingEntity.getActiveEffects()) {
-                if (instance.getEffect().value().isBeneficial()) {
-                    numPositiveEffects++;
-                }
-            }
-            Mth.clamp(numPositiveEffects, 0, 10);
-            AttributeInstance instanceAtkSpeed = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_SPEED);
-            AttributeInstance instanceAtkPower = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_DAMAGE);
-
-            if (instanceAtkPower != null && instanceAtkSpeed != null) {
-                instanceAtkPower.removeModifier(FiresEnderExpansion.id("eclipsed.power"));
-                instanceAtkPower.addOrUpdateTransientModifier(new AttributeModifier(FiresEnderExpansion.id("eclipsed.power"), numPositiveEffects * -0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
-
-                instanceAtkSpeed.removeModifier(FiresEnderExpansion.id("eclipsed.speed"));
-                instanceAtkSpeed.addOrUpdateTransientModifier(new AttributeModifier(FiresEnderExpansion.id("eclipsed.speed"), numPositiveEffects * -0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+    public static int getNumPositiveEffects(LivingEntity livingEntity){
+        //get number of positive effects
+        int numPositiveEffects = 0;
+        for (MobEffectInstance instance : livingEntity.getActiveEffects()) {
+            if (instance.getEffect().value().isBeneficial()) {
+                numPositiveEffects++;
             }
         }
-        return super.applyEffectTick(pLivingEntity, pAmplifier);
+        return numPositiveEffects;
     }
 
     @Override
@@ -50,10 +54,59 @@ public class EclipsedEffect extends MagicMobEffect {
         AttributeInstance instanceAtkSpeed = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_SPEED);
         AttributeInstance instanceAtkPower = pLivingEntity.getAttributes().getInstance(Attributes.ATTACK_DAMAGE);
 
+        if(buffTracking.containsKey(pLivingEntity)) {
+            buffTracking.remove(pLivingEntity);
+        }
+
         if (instanceAtkPower != null && instanceAtkSpeed != null) {
             instanceAtkPower.removeModifier(FiresEnderExpansion.id("eclipsed.power"));
             instanceAtkSpeed.removeModifier(FiresEnderExpansion.id("eclipsed.speed"));
         }
         super.onEffectRemoved(pLivingEntity, pAmplifier);
     }
+
+    @SubscribeEvent
+    public static void onEffectsGained(MobEffectEvent.Added event){
+        if(event.getEntity().hasEffect(EffectRegistry.ECLIPSED_EFFECT)){
+            //if we don't already track them then we need to manually figure out how many positive effects they have
+            if(!buffTracking.containsKey(event.getEntity())){
+                buffTracking.put(event.getEntity(), getNumPositiveEffects(event.getEntity()));
+            }
+            //increment if necessary
+            if(event.getEffectInstance().getEffect().value().isBeneficial()){
+                buffTracking.replace(event.getEntity(), buffTracking.get(event.getEntity()) + 1);
+            }
+            updateAttributeModifiers(event.getEntity());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEffectsRemoved(MobEffectEvent.Remove event){
+        if(event.getEntity().hasEffect(EffectRegistry.ECLIPSED_EFFECT)){
+            if(event.getEffectInstance().getEffect().value().isBeneficial()){
+                if(buffTracking.containsKey(event.getEntity())) {
+                    buffTracking.replace(event.getEntity(), buffTracking.get(event.getEntity()) - 1);
+                }else{
+                    buffTracking.put(event.getEntity(), getNumPositiveEffects(event.getEntity()));
+                }
+            }
+            updateAttributeModifiers(event.getEntity());
+        }
+    }
+
+    //Got to account for it being cleared and for it expiring...
+    @SubscribeEvent
+    public static void onEffectsExpired(MobEffectEvent.Expired event){
+        if(event.getEntity().hasEffect(EffectRegistry.ECLIPSED_EFFECT)){
+            if(event.getEffectInstance().getEffect().value().isBeneficial()){
+                if(buffTracking.containsKey(event.getEntity())) {
+                    buffTracking.replace(event.getEntity(), buffTracking.get(event.getEntity()) - 1);
+                }else{
+                    buffTracking.put(event.getEntity(), getNumPositiveEffects(event.getEntity()));
+                }
+            }
+        }
+        updateAttributeModifiers(event.getEntity());
+    }
+
 }
