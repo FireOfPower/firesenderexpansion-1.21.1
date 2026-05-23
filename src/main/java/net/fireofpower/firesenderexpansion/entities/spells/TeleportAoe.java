a912ecff46ee67f4a3d22949b1ad5efff0d24a03
FireOfPower <fireofpower333@gmail.com>
2026-05-23 16:14:10 -0400

    Renamed TBATE and Displacement Cage is now harsher

--- src/main/java/net/fireofpower/firesenderexpansion/entities/spells/TeleportAoe.java
+++ src/main/java/net/fireofpower/firesenderexpansion/entities/spells/TeleportAoe.java
@@ -5,16 +5,19 @@ import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.util.Utils;
 import io.redspace.ironsspellbooks.entity.mobs.AntiMagicSusceptible;
 import io.redspace.ironsspellbooks.entity.spells.AoeEntity;
-import net.acetheeldritchking.aces_spell_utils.network.AddShaderEffectPacket;
-import net.acetheeldritchking.aces_spell_utils.network.RemoveShaderEffectPacket;
-import net.fireofpower.firesenderexpansion.FiresEnderExpansion;
+import net.fireofpower.firesenderexpansion.registries.EffectRegistry;
 import net.fireofpower.firesenderexpansion.registries.EntityRegistry;
 import net.fireofpower.firesenderexpansion.registries.SpellRegistries;
 import net.fireofpower.firesenderexpansion.util.ModTags;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -26,19 +29,22 @@ import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.network.PacketDistributor;
+import net.neoforged.neoforge.event.entity.EntityTeleportEvent;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+@EventBusSubscriber
 public class TeleportAoe extends AoeEntity implements AntiMagicSusceptible {
-    private static final EntityDataAccessor<Float> RADIUS = SynchedEntityData.defineId(TeleportAoe.class, EntityDataSerializers.FLOAT);
     private static final EntityDataAccessor<Integer> DURATION = SynchedEntityData.defineId(TeleportAoe.class, EntityDataSerializers.INT);
     private float tpRadius = 0.5f;
-    private List<ServerPlayer> trackedShaderTargets = new ArrayList<>();
+    private final List<LivingEntity> trackedTargets = new ArrayList<>();
 
     public TeleportAoe(EntityType<? extends Projectile> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -51,47 +57,43 @@ public class TeleportAoe extends AoeEntity implements AntiMagicSusceptible {
 
     @Override
     public void tick() {
-        this.level().getEntitiesOfClass(LivingEntity.class,
-                        this.getBoundingBox()
-                                .inflate(1))
-                .stream()
-                .filter(liv -> liv.distanceTo(this) > getRadius() - tpRadius && liv.distanceTo(this) < getRadius() + tpRadius &&
-                        !Objects.equals(liv, this.getOwner()))
-                .forEach(e -> {
-                    if(!e.getType().is(ModTags.DISPLACEMENT_CAGE_IMMUNE) || e instanceof ServerPlayer serverPlayer && !(serverPlayer.isCreative() || serverPlayer.isSpectator()) && !e.getType().is(ModTags.DISPLACEMENT_CAGE_IMMUNE)) {
-                        //do the teleporty stuff
-                        Vec3 distFromCircleCenter = new Vec3((float) (e.position().x - this.position().x), 0, (float) (e.position().z - this.position().z));
-                        NeoForge.EVENT_BUS.post(new SpellTeleportEvent(SpellRegistries.DISPLACEMENT_CAGE.get(), e, e.position().x, e.position().y, e.position().z));
-                        Vec3 dest = e.position().subtract(new Vec3(distFromCircleCenter.x, 0, distFromCircleCenter.z).multiply(1.75, 1, 1.75));
-                        Utils.handleSpellTeleport(SpellRegistries.DISPLACEMENT_CAGE.get(), e, dest);
-                    }
-                });
 
         //get everyone sorta nearby (it's a square instead of a circle with radius dimensions)
-        List<ServerPlayer> targets = this.level().getEntitiesOfClass(ServerPlayer.class, new AABB(this.getX() - getRadius(), this.getY() - getRadius(), this.getZ() - getRadius(), this.getX() + getRadius(), this.getY() + getRadius(), this.getZ() + getRadius()));
+        List<LivingEntity> targets = this.level().getEntitiesOfClass(LivingEntity.class, new AABB(this.getX() - getRadius(), this.getY() - getRadius(), this.getZ() - getRadius(), this.getX() + getRadius(), this.getY() + getRadius(), this.getZ() + getRadius()));
         for(int i = 0; i < targets.size(); i++) {
+            //update the trackedTargets list
+            Vec3 distFromCircleCenter = new Vec3((float) (targets.get(i).position().x - this.position().x), 0, (float) (targets.get(i).position().z - this.position().z));
             //if we're not already tracking them, then start tracking them and apply the shader
-            if(!trackedShaderTargets.contains(targets.get(i))){
-                trackedShaderTargets.add(targets.get(i));
-                if(distanceTo(targets.get(i)) < getRadius()){
-                    PacketDistributor.sendToPlayer(trackedShaderTargets.get(i), new AddShaderEffectPacket(FiresEnderExpansion.MODID, "shaders/pink_shader.json"));
-                    level().playSound((Player) null, targets.get(i).position().x, targets.get(i).position().y, targets.get(i).position().z, SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 2.0F, 1.0F);
+            if(!trackedTargets.contains(targets.get(i)) && distFromCircleCenter.horizontalDistance() < getRadius()){
+                trackedTargets.add(targets.get(i));
+                level().playSound((Player) null, targets.get(i).position().x, targets.get(i).position().y, targets.get(i).position().z, SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 2.0F, 1.0F);
+            }
+        }
+
+        for(int i = 0; i < trackedTargets.size(); i++){
+            LivingEntity tracked = trackedTargets.get(i);
+            Vec3 distFromCircleCenter = new Vec3((float) (tracked.position().x - this.position().x), 0, (float) (tracked.position().z - this.position().z));
+            if(distFromCircleCenter.horizontalDistance() > getRadius()) {
+                if (!tracked.getType().is(ModTags.DISPLACEMENT_CAGE_IMMUNE) || tracked instanceof ServerPlayer serverPlayer && !(serverPlayer.isCreative() || serverPlayer.isSpectator()) && !serverPlayer.getType().is(ModTags.DISPLACEMENT_CAGE_IMMUNE)) {
+                    //do the teleporty stuff
+                    Vec3 dest = position().add(position().subtract(tracked.position()).normalize().multiply(getRadius() * 0.99,0,getRadius() * 0.99)).subtract(0,position().subtract(tracked.position()).y,0);
+                    BlockPos output = new BlockPos((int) Math.round(dest.x), (int) Math.round(dest.y), (int) Math.round(dest.z));
+                    while(!level().isEmptyBlock(output)){
+                        dest = dest.add(0,1,0);
+                        output = output.offset(0,1,0);
+                    }
+                    if(!Utils.handleSpellTeleport(SpellRegistries.DISPLACEMENT_CAGE.get(), tracked, dest)) {
+                        if (trackedTargets.contains(tracked)){
+                            trackedTargets.remove(tracked);
+                        }
+                    }
                 }
             }
         }
-        //stop tracking people that leave our range
-        for(int i = 0; i < trackedShaderTargets.size(); i++){
-            if(distanceTo(trackedShaderTargets.get(i)) > getRadius()){
-                PacketDistributor.sendToPlayer(trackedShaderTargets.get(i), new RemoveShaderEffectPacket());
-                trackedShaderTargets.remove(i);
-                i--;
-            }
-        }
+
         if(tickCount > getDuration()){
-            //when we are done, remove the shader from everyone who's being tracked
-            for(int i = 0; i < trackedShaderTargets.size(); i++){
-                PacketDistributor.sendToPlayer(trackedShaderTargets.get(i), new RemoveShaderEffectPacket());
-                trackedShaderTargets.remove(i);
+            for(int i = 0; i < trackedTargets.size(); i++){
+                trackedTargets.remove(i);
                 i--;
             }
             discard();
@@ -99,9 +101,13 @@ public class TeleportAoe extends AoeEntity implements AntiMagicSusceptible {
         super.tick();
     }
 
-    @Override
-    protected boolean canHitEntity(Entity pTarget) {
-        return !pTarget.isSpectator() && pTarget.isAlive() && pTarget.isPickable();
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void allowFreeTeleportation(EntityTeleportEvent event) {
+        TeleportAoe aoe = new TeleportAoe(event.getEntity().level());
+        List<TeleportAoe> nearby = event.getEntity().level().getEntitiesOfClass(TeleportAoe.class,new AABB(new Vec3(event.getEntity().position().subtract(aoe.getRadius(),aoe.getRadius(),aoe.getRadius()).toVector3f()), new Vec3(event.getEntity().position().add(aoe.getRadius(),aoe.getRadius(),aoe.getRadius()).toVector3f())));
+        for(int i = 0; i < nearby.size(); i++){
+            nearby.get(i).trackedTargets.remove(event.getEntity());
+        }
     }
 
     @Override
@@ -126,21 +132,16 @@ public class TeleportAoe extends AoeEntity implements AntiMagicSusceptible {
 
     @Override
     public void onAntiMagic(MagicData magicData) {
-        for(int i = 0; i < trackedShaderTargets.size(); i++){
-            PacketDistributor.sendToPlayer(trackedShaderTargets.get(i), new RemoveShaderEffectPacket());
-            trackedShaderTargets.remove(i);
+        for(int i = 0; i < trackedTargets.size(); i++){
+            trackedTargets.remove(i);
             i--;
         }
+        discard();
     }
 
     @Override
     public float getRadius() {
-        return this.entityData.get(RADIUS);
-    }
-
-    @Override
-    public void setRadius(float radius) {
-        this.entityData.set(RADIUS,radius);
+        return 6;
     }
 
     @Override
@@ -155,7 +156,6 @@ public class TeleportAoe extends AoeEntity implements AntiMagicSusceptible {
     @Override
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
-        builder.define(RADIUS, 6f);
         builder.define(DURATION,0);
     }
 }
